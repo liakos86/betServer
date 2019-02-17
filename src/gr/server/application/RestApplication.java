fa36fda276eac35a1e556a275bda0e63d053fc60
Liakos <liakos86@gmail.com>
2019-02-17 18:52:58 +0200

    deleted logs

--- src/gr/server/application/RestApplication.java
+++ src/gr/server/application/RestApplication.java
@@ -3,48 +3,24 @@ package gr.server.application;
 import gr.server.impl.service.MyBetOddsServiceImpl;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 import javax.ws.rs.core.Application;
 
 
 /**
- * 
+ * Imports the classes that will provide the rest service.
  */
 public class RestApplication  extends Application
 {
-//    public Set<Class<?>> getClasses()
-//    {
-//        Set<Class<?>> s = new HashSet<Class<?>>();
-//        s.add(PersonServiceImpl.class);
-//        return s;
-//    }
-    
 
     @Override
     public Set<Class<?>> getClasses() {
-        
         Set<Class<?>> resources = new HashSet<Class<?>>();
-        
         System.out.println("REST configuration starting: getClasses()");      
-        
         resources.add(MyBetOddsServiceImpl.class);
-        
-        //features
-        //this will register Jackson JSON providers
-        ///resources.add(org.glassfish.jersey.jackson.JacksonFeature.class);
-        //we could also use this:
-        //resources.add(com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider.class);
-        
-        //instead let's do it manually:
-        ///resources.add(MyJacksonProvider.class);
-        //==> we could also choose packages, see below getProperties()
-        
         System.out.println("REST configuration ended successfully.");
-        
         return resources;
     }
     
@@ -52,9 +28,5 @@ public class RestApplication  extends Application
     public Set<Object> getSingletons() {
         return Collections.emptySet();
     }
-    
    
 }
-    
-    
-    
